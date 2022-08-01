bd5d87d38ab629d8f667e9bdb8a4af2b2887c1c6
Pablo Torres <comodoro_21@hotmail.com>
2022-08-01 17:25:31 -0500

    change values of test

--- bin/index.js
+++ bin/index.js
@@ -6,20 +6,25 @@ const yargs = require("yargs");
 var fs = require('fs');
 
 const options = yargs
-    .usage("Usage: -c <domain> -p <port>")
-    .option("c", { alias: "domain", describe: "Your domain or subdomain", type: "string", demandOption: true })
+    .usage("Usage: -d <domain> -p <port>")
+    .option("d", { alias: "domain", describe: "Your domain or subdomain", type: "string", demandOption: true })
     .option("p", { alias: "port", describe: "Port", type: "string", demandOption: true })
     .argv;
 const greeting = `Hello, ${options.domain}! ${options.port}`;
 let template = templateNginx(options.domain, options.port)
 console.log(template)
-fs.writeFile(`/etc/nginxTest/sites-avaliable/${options.domain}`, template, (err)=> {
+fs.writeFile(`/etc/nginx/sites-avaliable/${options.domain}`, template, (err) => {
     if (err) throw err;
     console.log('File is created successfully.');
-    executeCommandLine(`ln -s /etc/nginxTest/sites-avaliable/${options.domain}  /etc/nginxTest/sites-enable/.`).then(msg=>{
+    executeCommandLine(`ln -s /etc/nginx/sites-avaliable/${options.domain}  /etc/nginxTest/sites-enable/.`).then(msg => {
+        executeCommandLine(`sudo certbot --nginx -d ${options.domain}`).then(msg=>{
+            console.log(msg)
+        }).catch(err=>{
+            console.log(err)
+        })
         console.log(msg)
-    }).catch(err=>{
-
+    }).catch(err => {
+        console.log(err)
     })
 });
 
