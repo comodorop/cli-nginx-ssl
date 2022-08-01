035b60df91fd3a7072ebf0f14c5d8125f096b275
Pablo Torres <comodoro_21@hotmail.com>
2022-08-01 17:20:13 -0500

    Remove file test

--- test.js
+++
@@ -1,9 +0,0 @@
-const exec = require('child_process').exec;
-const myShellScript = exec('ls -l');
-myShellScript.stdout.on('data', (data)=>{
-    console.log(data); 
-    // do whatever you want here with data
-});
-myShellScript.stderr.on('data', (data)=>{
-    console.error(data);
-});
